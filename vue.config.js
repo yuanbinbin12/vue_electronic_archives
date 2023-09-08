d901cbc3f1ca24d4fdfeedf5ba77d804dc765d0d
ybb <447422351@qq.com>
2023-09-08 16:17:34 +0800

    修改代理

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,16 @@ module.exports = defineConfig({
   publicPath: './',
   transpileDependencies: true,
   lintOnSave:false,
+  
   devServer:{
     proxy:{
       '/OSPApplication':{
         target:'http://localhost:8080',
         ws: true, //websocket服务端和通信端的通信方式  用于支持websocket
         changeOrigin: true, //跨域伪造  端口号说慌
+      //   pathRewrite: {
+      //     '^/OSPApplication': '/0SPFileApplication'
+      // }
       }
     }
   }
